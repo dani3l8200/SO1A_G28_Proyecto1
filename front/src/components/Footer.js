938c1d962a5aed96ba72399ea43a5e691bc72c24
pabloandresarg <3004456600101@ingenieria.usac.edu.gt>
2021-03-09 22:45:50 -0600

    footer

---
+++ front/src/components/Footer.js
@@ -0,0 +1,9 @@
+import react from "react";
+
+export default function Footer(){ // los componentes tiene que comenzar con mayuscula
+    return (
+        <>
+            <h2> GRUPO 28  SOPES 1 </h2>
+        </>
+    );
+}
