a9270046e9415fc15f4c606757e3c43888e37907
PabloAndresArg <PabloAndresArg@gmail.com>
2021-03-23 14:58:22 -0600

    limitando a 50 , y hora en la grafica de la ram

--- front/src/components/reportes_/reportes.js
+++ front/src/components/reportes_/reportes.js
@@ -1,10 +1,5 @@
 // en este componente van a ir la lista de los reportes
 import Rep1 from './reporte1_/Reporte1';
-
-import Rep2 from './reporte2_/Reporte2';
-import Rep4 from './reporte4_/Reporte4';
-import Rep6 from './reporte6_/Reporte6';
-import Rep7 from './reporte7_/Reporte7';
 export default function Monitor(){
 
 return (
